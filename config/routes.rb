b6e2d8c83afe17f8f93019a39b229505ecdcc40d
Pauloparakleto <alparakleto@gmail.com>
2020-08-03 03:43:55 -0300

    Routes nested for issue events

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   post "webhook/", to: 'webhook#receiver'
+
+  resources :issue do
+    resources :events, only: [:index, :show]
+  end
+  resources :events, only: [:index, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
