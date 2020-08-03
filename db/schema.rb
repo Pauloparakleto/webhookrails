def42761f9f466c9ff72e0db95a47b9bbd9e0fc7
Pauloparakleto <alparakleto@gmail.com>
2020-08-03 02:06:11 -0300

    Schema migrate event

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_02_042749) do
+ActiveRecord::Schema.define(version: 2020_08_03_032727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.integer "issue_id"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "issues", force: :cascade do |t|
     t.integer "number"
