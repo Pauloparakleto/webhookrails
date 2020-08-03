b42e86fcc98f5bf3571547f9fecd9e548c24c83c
Pauloparakleto <alparakleto@gmail.com>
2020-08-03 17:10:41 -0300

    routes for issues show and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   post "webhook/", to: 'webhook#receiver'
 
+  get 'issues/', to: 'issues#index'
+  get 'issues/:id', to: 'issues#show'
   resources :issue do
     resources :events, only: [:index, :show]
   end
