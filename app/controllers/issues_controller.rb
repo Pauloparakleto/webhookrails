b69048839eabd82b930fc08f535f4ff4c258ccf5
Pauloparakleto <alparakleto@gmail.com>
2020-08-03 17:11:24 -0300

    Add index and show to issues controller to render json

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,5 +1,11 @@
 class IssuesController < ApplicationController
     def index
         @issues = Issue.all
+        render json: @issues
+    end
+
+    def show
+        @issue = Issue.find(params[:id])
+        render json: @issue
     end
 end
