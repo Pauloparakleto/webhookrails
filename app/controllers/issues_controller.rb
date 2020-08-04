c881c32b4b0022e4d5924250783a806923a1174d
Pauloparakleto <alparakleto@gmail.com>
2020-08-04 14:44:08 -0300

    Handle activerecord contentnotfound exception on issue show controller

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,4 +1,7 @@
 class IssuesController < ApplicationController
+
+    rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+
     def index
         @issues = Issue.all
         render json: @issues
@@ -7,5 +10,9 @@ class IssuesController < ApplicationController
     def show
         @issue = Issue.find(params[:id])
         render json: @issue
+    end
+
+    def handle_record_not_found
+        render json: "{status: 404, response: Content not found, id: #{params[:id]}}"
     end
 end
