da005b5528dac070dd291bcb252ca190b7691f22
Pauloparakleto <alparakleto@gmail.com>
2020-08-04 15:53:27 -0300

    Routes for plural and singular

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,20 @@
 Rails.application.routes.draw do
   post "webhook/", to: 'webhook#receiver'
 
+  get 'issue/', to: 'issues#index'
   get 'issues/', to: 'issues#index'
+  get 'issue/:id', to: 'issues#show'
   get 'issues/:id', to: 'issues#show'
+
+  get 'issue/:issue_id/event/', to: 'events#index'
+  get 'issue/:issue_id/events/', to: 'events#index'
+  get 'issues/:issue_id/event/', to: 'events#index'
+  get 'issues/:issue_id/events/', to: 'events#index'
+
+  get 'issues/:issue_id/event/:id', to: 'events#show'
+  get 'issues/:issue_id/events/:id', to: 'events#show'
+
+
   resources :issue do
     resources :events, only: [:index, :show]
   end
