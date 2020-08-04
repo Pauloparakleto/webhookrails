7e2b719fe038ac843103e4f02d70e492b310920c
Pauloparakleto <alparakleto@gmail.com>
2020-08-04 14:49:48 -0300

    Standard rescue response for index and show action to issue controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,6 @@
 class EventsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :handle_record_not_found
+
     def index
         @events = Event.all
         render json: @events
@@ -9,6 +11,9 @@ class EventsController < ApplicationController
         render json: @event
     end
 
+    def handle_record_not_found
+        render json: "{status: 404, response: Content not found}"
+    end
     #def issue_events
         #@issue = Issue.find(params[:issue_id])
         #@events = Event.all
